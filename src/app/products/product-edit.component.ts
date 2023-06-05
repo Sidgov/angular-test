02be7f56b988ac12025f890046debf2167d6e1ab
Paul Horcholle <horchollepaul@gmail.com>
2023-06-05 21:45:08 +0200

    a

--- src/app/products/product-edit.component.ts
+++ src/app/products/product-edit.component.ts
@@ -1,6 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { IProduct } from './product';
-import { FormControl, FormGroup } from '@angular/forms';
+import { IMessages, IProduct, IValidationMessages, Inputs, isIProduct } from './product';
+import { AbstractControl, FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { debounceTime } from 'rxjs';
+
+function ratingRange(min: number, max: number): ValidatorFn {
+  return (c: AbstractControl): { [key: string]: boolean } | null  => {
+    if (c.value !== null && (isNaN(c.value)) || c.value < min || c.value > max) {
+      return { 'range': true };
+    }
+    return null;
+  }
+}
 
 @Component({
   selector: 'pm-product-edit',
@@ -10,17 +20,75 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class ProductEditComponent implements OnInit {
   productForm!: FormGroup;
   product: IProduct | undefined;
-  
-  constructor() {
-    console.log('test')
+
+  messages: IMessages = {
+    productName: '',
+    productCode: '',
+    price: '',
+    starRating: ''
   }
 
+  private validationMessages: IValidationMessages = {
+    productName: {
+      required: 'Please enter a product name.',
+      minlength: 'The product name must be longer than 3 characters.',
+    },
+    productCode: {
+      required: 'Please enter a product code.',
+    },
+    price: {
+      required: 'Please enter a price.',
+    },
+    starRating: {
+      range: 'A star rating must be between 1 and 5'
+    },
+  }
+  
+  constructor(private fb : FormBuilder) {}
+
   ngOnInit(): void {
-    this.productForm = new FormGroup({
-      productName: new FormControl(),
-      productCode: new FormControl(),
-      price: new FormControl(),
-      starRating: new FormControl(),
+    if (isIProduct(history.state)) {
+      this.product = history.state;
+    }
+
+    this.productForm = this.fb.group({
+      productName: [this.product?.productName, [Validators.required, Validators.minLength(3)]],
+      productCode: [this.product?.productCode, [Validators.required]],
+      price: [this.product?.price, [Validators.required]],
+      starRating: [this.product?.starRating, [Validators.required, ratingRange(1, 5)]],
     });
+
+    const productNameControl = this.productForm.get(Inputs.PRODUCT_NAME);
+    productNameControl?.valueChanges.pipe(
+      debounceTime(1000)
+    ).subscribe(
+      () => this.setMessage(Inputs.PRODUCT_NAME, productNameControl)
+    );
+    const productCodeControl = this.productForm.get(Inputs.PRODUCT_CODE);
+    productCodeControl?.valueChanges.pipe(
+      debounceTime(1000)
+    ).subscribe(
+      () => this.setMessage(Inputs.PRODUCT_CODE, productCodeControl)
+    );
+    const priceControl = this.productForm.get(Inputs.PRICE);
+    priceControl?.valueChanges.pipe(
+      debounceTime(1000)
+    ).subscribe(
+      () => this.setMessage(Inputs.PRICE, priceControl)
+    );
+    const starRatingControl = this.productForm.get(Inputs.STAR_RATING);
+    starRatingControl?.valueChanges.pipe(
+      debounceTime(1000)
+    ).subscribe(
+      () => this.setMessage(Inputs.STAR_RATING, starRatingControl)
+    );
+  }
+
+  setMessage(input: Inputs, c: AbstractControl): void {
+    this.messages[input] = '';
+    if ((c.touched || c.dirty) && c.errors) {
+      this.messages[input] = Object.keys(c.errors).map(
+        key => this.validationMessages[input][key]).join(' ');
+    }
   }
 }
--- src/app/products/product.ts
+++ src/app/products/product.ts
@@ -1,3 +1,5 @@
+import { ValidationErrors } from "@angular/forms"
+
 export interface IProduct {
   productId: number
   productName: string
@@ -7,4 +9,32 @@ export interface IProduct {
   price: number
   starRating: number
   imageUrl: string
+}
+
+export function isIProduct(object: any): object is IProduct {
+  return 'productId' in object
+    && 'productName' in object
+    && 'productCode' in object
+    && 'releaseDate' in object
+    && 'description' in object
+    && 'price' in object
+    && 'starRating' in object
+    && 'imageUrl' in object;
+} 
+
+export enum Inputs {
+  PRODUCT_NAME = 'productName',
+  PRODUCT_CODE = 'productCode',
+  PRICE = 'price',
+  STAR_RATING = 'starRating'
+}
+
+export type IMessages = {
+  [key in Inputs]: string
+}
+
+export type IValidationMessages = {
+  [key in Inputs]: {
+    [key: keyof ValidationErrors]: string,
+  }
 }
